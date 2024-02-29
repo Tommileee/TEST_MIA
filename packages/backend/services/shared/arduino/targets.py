e768a0b93d269e54e73024caac5d33c3f712fefa
Tommileee <104897621+Tommileee@users.noreply.github.com>
2024-02-29 12:40:11 +0100

    Delete packages directory

--- packages/backend/services/shared/arduino/targets.py
+++
@@ -1,7 +0,0 @@
-class Target:
-    MOTOR_LEFT = "motor-left"
-    MOTOR_RIGHT = "motor-right"
-    ARM_LEFT = "arm-left"
-    ARM_RIGHT = "arm-right"
-    HEAD_ROTATION = "head-rotation"
-    HEAD_EXTENSION = "head-extension"
