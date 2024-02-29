91a453d18d837a705ca5e1728851a1a3d02781d3
Tommileee <104897621+Tommileee@users.noreply.github.com>
2024-02-29 11:15:03 +0100

    Update camera_service.py

--- packages/backend/services/camera_service.py
+++ packages/backend/services/camera_service.py
@@ -1,7 +1,8 @@
 import cv2
+import os
 
 def capture_continuous(frequency, file_path):
-    cap = cv2.VideoCapture(0)  # 0 steht für die erste angeschlossene Kamera
+    cap = cv2.VideoCapture(0)
     
     if not cap.isOpened():
         print("Kann Kamera nicht öffnen")
@@ -10,13 +11,18 @@ def capture_continuous(frequency, file_path):
     while True:
         ret, frame = cap.read()
         if ret:
-            cv2.imwrite(file_path, frame)
+            try:
+                cv2.imwrite(file_path, frame)
+                print("Bild gespeichert bei:", file_path)  # Bestätigung hinzugefügt
+            except Exception as e:
+                print("Fehler beim Speichern des Bildes:", e)
         else:
             print("Kann Bild nicht erfassen")
-            break
-        
-        # Wartezeit, um die gewünschte Frequenz zu erreichen
+
         cv2.waitKey(int(1000/frequency))
 
 if __name__ == "__main__":
-    capture_continuous(24, "/shared/web/captured_image.jpeg")
+    # Stellen Sie sicher, dass der Pfad existiert oder fügen Sie Code hinzu, um ihn zu erstellen
+    file_path = "/shared/web/captured_image.jpeg"
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)  # Ordner erstellen, falls nicht vorhanden
+    capture_continuous(24, file_path)
